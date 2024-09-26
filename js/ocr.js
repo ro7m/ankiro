e112cf91ca3aaf5704373793e6ddd50988541927
ro7m <robin.mundra@gmail.com>
2024-09-26 23:38:32 +0530

    Update ocr.js

--- js/ocr.js
+++ js/ocr.js
@@ -1,5 +1,5 @@
 const performOCR = async (imageData) => {
-    const result = await Tesseract.recognize(imageData, {
+    const result = await Tesseract.recognize(imageData,'eng', {
         logger: m => console.log(m)
     });
     return result.data.text;

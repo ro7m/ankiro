3f098d3dd8555121dcb6cf7adcbf58aac09af86f
ro7m <robin.mundra@gmail.com>
2024-09-28 00:49:28 +0530

    Update ocr.js

--- js/ocr.js
+++ js/ocr.js
@@ -1,5 +1,4 @@
 // Load TensorFlow.js and the model
-import * as tf from '@tensorflow/tfjs';
 
 const VOCAB =
   "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~°£€¥¢฿àâéèêëîïôùûüçÀÂÉÈÊËÎÏÔÙÛÜÇ"
@@ -7,19 +6,18 @@ const VOCAB =
 // Function to load and predict with the TensorFlow model
 async function loadModel() {
   // Load the model (path can be a URL or local path for Node.js)
-  const model = await tf.loadGraphModel('models/crnn_mobilenet_v2/model.json'); 
+  const model = await Tensorflow.loadGraphModel('models/crnn_mobilenet_v2/model.json'); 
 
   return model;
 }
 
 async function preprocessImage(imageData) {
-  let image = tf.browser.fromPixels(imageData);
-  const resizedImage = tf.image.resizeBilinear(image, [128, 32]); // Resize to model input size
+  let image = Tensorflow.browser.fromPixels(imageData);
+  const resizedImage = Tensorflow.image.resizeBilinear(image, [128, 32]); // Resize to model input size
   const normalizedImage = resizedImage.div(255.0); // Normalize
   return normalizedImage.expandDims(0); // Add batch dimension
 }
 async function processPredictions(predictions) {
-  let decodedText = ''; 
   let probabilities = softmax(predictions, -1);
   let bestPath = unstack(argMax(probabilities, -1), 0);
   let blank = 126;
@@ -40,7 +38,6 @@ async function processPredictions(predictions) {
     words.push(collapsed);
   }
   return words;
-  return decodedText;
 }
 
 

7cfaed7063710bce2b1a80cb874f1c33a43c6b98
ro7m <robin.mundra@gmail.com>
2024-09-27 16:22:43 +0530

    Update ocr.js

--- js/ocr.js
+++ js/ocr.js
@@ -1,6 +1,32 @@
+// Load TensorFlow.js and the model
+const tf = require('@tensorflow/tfjs');
+
+// Function to load and predict with the TensorFlow model
+async function loadModel() {
+  // Load the model (path can be a URL or local path for Node.js)
+  const model = await tf.loadGraphModel('models/crnn_mobilenet_v2/model.json'); 
+
+  return model;
+}
+
+async function preprocessImage(imageData) {
+  let image = tf.browser.fromPixels(imageData);
+  const resizedImage = tf.image.resizeBilinear(image, [128, 32]); // Resize to model input size
+  const normalizedImage = resizedImage.div(255.0); // Normalize
+  return normalizedImage.expandDims(0); // Add batch dimension
+}
+async function processPredictions(predictions) {
+  let decodedText = ''; 
+  return decodedText;
+}
+
+
+
 const performOCR = async (imageData) => {
-    const result = await Tesseract.recognize(imageData,'eng', {
-        logger: m => console.log(m)
-    });
-    return result.data.text;
+
+  const model = await loadModel();
+  const preprocessedImage = await preprocessImage(imageData);
+  const predictions = model.predict(preprocessedImage);
+  const output = await processPredictions(predictions);
+  return output;
 };
